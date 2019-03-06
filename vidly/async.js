0148bc97cc212ab91e5e8538eeac35ffe6af2002
zelzhan <elzhan.zeinulla@nu.edu.kz>
2019-03-06 14:51:34 +0600

    convert all callabacks to promises

--- vidly/async.js
+++ vidly/async.js
@@ -1,6 +1,30 @@
 console.log('Before');
-getUser(11, logUser);
+getUser(11).then( (user) => {
+	console.log(user);
+	getCourses(user).then((courses) => {
+		console.log(courses);
+	})
+});
 console.log('After');
+
+function getUser(id) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Imitation of request to the database...')
+			resolve({id: id, name: "Elzhan"});
+		}, 1000)		
+	})
+	
+}
+
+function getCourses(user){
+	console.log('Calling the registrar API...')
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+		resolve(['course1', 'course2', 'course3']);	
+		}, 1500)	
+	})
+}
 
 function logUser(user){
 	console.log(user);
@@ -11,18 +35,5 @@ function logCourses(courses){
 	console.log(courses);
 }
 
-function getUser(id, callback) {
-	setTimeout(() => {
-		console.log('Imitation of request to the database...')
-		callback({id: id, name: "Elzhan"});
-	}, 1000)
-}
 
 
-function getCourses(user, callback){
-	console.log('Calling the registrar API...')
-	setTimeout(() => {
-		callback(['course1', 'course2', 'course3']);	
-	}, 1500)
-	
-}
